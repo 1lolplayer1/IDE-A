577cd879a1836e115adf3850ddfafda00a1fd62d
1lolplayer1 <11lolplayer11@gmail.com>
2023-04-22 12:40:51 +0400

    file manager works good now + deleted some files

--- main_linux.py
+++ main_linux.py
@@ -7,21 +7,23 @@ from tkinter.messagebox import showinfo
 #import ctypes
 import re
 import os
+import idlelib.colorizer as ic
+import idlelib.percolator as ip
 from tkinter.filedialog import asksaveasfilename, askopenfilename, askdirectory
 import threading
-import customtkinter
+import customtkinter as ctk
 
 
 
 # Increas Dots Per inch so it looks sharper
 #ctypes.windll.shcore.SetProcessDpiAwareness(True)
-customtkinter.set_appearance_mode("dark")  
-customtkinter.set_default_color_theme("blue")
+ctk.set_appearance_mode("dark")  
+ctk.set_default_color_theme("blue")
 
 
 
 # Setup Tkinter
-root = customtkinter.CTk()
+root = ctk.CTk()
 root.geometry('800x600')
 root.title("IDE-A (BETA 0.00001)")
 title = "IDE-A (Beta 0.00001)"
@@ -53,10 +55,10 @@ def save(s=0):
         f_path = asksaveasfilename(filetypes=[('Python Files', '*.py')])
     else:
         f_path = file_path
-    with open(path, 'w') as file:
+    with open(f_path, 'w') as file:
         code = editArea.get('1.0', END)
         file.write(code)
-        set_file_path(path)
+        set_file_path(f_path)
         root.title(f'IDE-A (Beta 0.00001) {file_path}')
 
 def save_as(s=0):
@@ -76,6 +78,7 @@ def run(s=0):
         save_prompt = Toplevel()
         text = Label(save_prompt, text='Please save your code')
         text.pack()
+        save()
         return
     else:
         #Windows one
@@ -92,8 +95,19 @@ def run(s=0):
 
 
 
+
+
+
+
+
+
+
+
+
+
+
 # Register Changes made to the Editor Content
-def changes(event=None):
+'''def changes(event=None):
     global previousText
 
     # If actually no changes have been made stop / return the function
@@ -114,8 +128,8 @@ def changes(event=None):
             i+=1
 
     previousText = editArea.get('1.0', END) 
-
-def search_re(pattern, text, groupid=0):
+'''
+'''def search_re(pattern, text, groupid=0):
     matches = []
 
     text = text.splitlines()
@@ -127,7 +141,7 @@ def search_re(pattern, text, groupid=0):
             )
 
     return matches
-
+'''
 
 def rgb(rgb):
     return "#%02x%02x%02x" % rgb
@@ -144,19 +158,20 @@ string = rgb((234, 162, 95))
 function = rgb((95, 211, 234))
 background = rgb((40, 41, 35))
 tree = rgb((42, 42, 48))
-
+variables = rgb((148, 215, 71))
 font = 'Consolas 15'
 
 
 # Define a list of Regex Pattern that should be colored in a certain way
-repl = [
+'''repl = [
     ['(^| )(False|None|True|and|as|assert|async|await|break|class|continue|def|del|elif|else|except|finally|for|from|global|if|import|in|is|lambda|nonlocal|not|or|pass|raise|return|try|while|with|yield)($| )', keywords],
     ['".*?"', string],
     ['\'.*?\'', string],
     ['#.*?$', comments],
-    ['print|input|', function]
+    ['print|input|', function],
+    #['str|int|byte|float', variables]
 ]
-
+'''
 
 
 ########################################
@@ -165,33 +180,42 @@ repl = [
 file_tree = Frame(root)
 file_tree.pack(side='left', fill=Y, expand=NO)
 def open_dir():
+    global abspath
     for i in tree.get_children():
         tree.delete(i)
     path = askdirectory()
     abspath = os.path.abspath(path)
     root_node = tree.insert('', 'end', text=abspath, open=True)
     process_directory(root_node,abspath)
-def process_directory( parent, path):
 
-        for p in os.listdir(path):
-            abspath = os.path.join(path, p)
-            isdir = os.path.isdir(abspath)
-            oid = tree.insert(parent, 'end', text=p, open=False)
-            if isdir:
-                process_directory(oid, abspath)
 
+filepaths = {}
+def process_directory(parent, path):
+    for p in os.listdir(path):
+        abspath = os.path.join(path, p)
+        isdir = os.path.isdir(abspath)
+        oid = tree.insert(parent, 'end', text=p, open=False)
+        filepaths[oid] = abspath  # save the full pathname
+        if isdir:
+            process_directory(oid, abspath)
 
 def Open_file_from_list_box(value):
-    global file
-    item_id = tree.selection()
-    file = tree.item(item_id, 'text') # get the filename from 'text' option
-    filepath = os.path.join(value,file)     
-    editArea.delete(1.0,END)
-    with open(filepath,"r") as f:
-        editArea.insert(1.0,f.read())
+    global file_path
+    file_path = ''
+    try:
+        item_id = tree.selection()[0]
+        file_path = filepaths[item_id] # get the full pathname
+        root.title(f"IDE-A (BETA 0.00001) {file_path}")
+        editArea.delete(1.0,END)
+        with open(file_path,"r") as f:
+            editArea.insert(1.0,f.read())
+    except Exception as ex:
+        print(ex.__class__.__name__, ex)
+
+
 
 tree = ttk.Treeview(file_tree)
-tree.pack(expand=YES,fill=BOTH)
+tree.pack(expand=YES, fill=BOTH)
 path = '.'
 tree.heading('#0', text=path, anchor='w')
 abspath = os.path.abspath(path)
@@ -225,8 +249,9 @@ def check_file_path():
 control_bttn= Button(controll_panel, image=Run_bttn, command=check_file_path,
 borderwidth=0)
 control_bttn.pack(side='right', pady=0.5)
-open_dir = Button(controll_panel,text="Open Directory",command=open_dir)
-open_dir.pack(side='left', fill='y')
+
+open_dir_bttn = Button(controll_panel,text="Open Directory",command=open_dir)
+open_dir_bttn.pack(side='left', fill='y')
 
 gapFrame = Frame(root, width=1)
 gapFrame.pack(side='left', fill='y')
@@ -248,6 +273,60 @@ editArea = Text(
 )
 
 editArea.pack(fill=BOTH, expand=1, side='right')
+
+cdg = ic.ColorDelegator()
+cdg.prog = re.compile(r'\b(?P<MYGROUP>tkinter)\b|' + ic.make_pat().pattern, re.S)
+cdg.idprog = r"(?<!class)\s+(\w+)"
+
+cdg.tagdefs['MYGROUP'] = {'foreground': '#7F7F7F', 'background': background}
+
+# These five lines are optional. If omitted, default colours are used.
+cdg.tagdefs['COMMENT'] = {'foreground': '#007F00 ', 'background': background}
+cdg.tagdefs['KEYWORD'] = {'foreground': '#27b1dd', 'background': background}
+cdg.tagdefs['BUILTIN'] = {'foreground': '#dddd22', 'background': background}
+cdg.tagdefs['STRING'] = {'foreground': '#8b9b40', 'background': background}
+cdg.tagdefs['DEFINITION'] = {'foreground': '#27b9b9', 'background': background}
+
+
+ip.Percolator(editArea).insertfilter(cdg)
+
+
+'''
+CHARBLUE = '#007F00 '
+CLOUD = '#4aded9 '
+CLOUD2 = '#47c2be '
+LEMON = '#e1ec34 '
+OVERCAST = '#dddd22 '
+PUMPKIN = '#ea9f13'
+STORMY = '#307ab4 '
+FIRE = '#ed7110 '
+DK_SEAFOAM = '#27b1dd '
+PURPLE = '#8623c2 '
+#test
+IDPROG = r"(?<!class)\s+(\w+)"
+
+TAGDEFS   = {   'COMMENT'    : {'foreground': CHARBLUE  , 'background': background},
+                'TYPES'      : {'foreground': CLOUD2    , 'background': background},
+                'NUMBER'     : {'foreground': LEMON     , 'background': background},
+                'BUILTIN'    : {'foreground': OVERCAST  , 'background': background},
+                'STRING'     : {'foreground': PUMPKIN   , 'background': background},
+                'DOCSTRING'  : {'foreground': STORMY    , 'background': background},
+                'EXCEPTION'  : {'foreground': CLOUD2    , 'background': background, 'font':'bold'},
+                'DEFINITION' : {'foreground': FIRE    , 'background': background, 'font':'bold'},
+                'DECORATOR'  : {'foreground': CLOUD2    , 'background': background, 'font':'italic'},
+                'INSTANCE'   : {'foreground': CLOUD     , 'background': background, 'font':'italic'},
+                'KEYWORD'    : {'foreground': DK_SEAFOAM, 'background': background, 'font':'bold'},
+                'CLASSDEF'   : {'foreground': PURPLE    , 'background': background, 'font':'bold'},
+            }
+
+cd         = ic.ColorDelegator()
+cd.idprog  = re.compile(IDPROG, re.S)
+cd.tagdefs = {**cd.tagdefs, **TAGDEFS}
+ip.Percolator(editArea).insertfilter(cd)'''
+
+
+
+
 
 class Terminal:
     def __init__(self, master):
@@ -291,7 +370,23 @@ class Terminal:
 terminal = Terminal(main_frame)
 
 
+def open_settings():
+    settings_window = ctk.CTk()
+    settings_window.title("IDE-A (Options)")
+    settings_window.geometry('600x400')
+    settings_window.state('normal')
 
+    string_label = ctk.CTkLabel(settings_window, text='Nothing is here dude :) \nYet...', pady=100, font=('Consolas', 15))
+    string_label.pack()
+    #string_label = ctk.CTkLabel(settings_window, text='Choose String color: ')
+    #string_label.pack()
+
+    #option_box = ctk.CTkOptionMenu(settings_window, values=["yellow", 'green', 'blue', 'red'])
+    #option_box.pack()
+    #option_box.set('yellow')
+
+
+    
 
 ########################################
 #GAP
@@ -301,14 +396,17 @@ gap.pack(side='left', fill='y', expand=NO)
 
 
 
-
-
 menu_bar = Menu(root)
 
 
 notes_bar = Menu(menu_bar, tearoff=0)
 notes_bar.add_command(label='PLZ NOTE THAT IF U CLICK "SAVE AS" THEN WRITE -|.py|- EXTENSION URSELF AT THE END')
 menu_bar.add_cascade(label='!NOTE!', menu=notes_bar)
+
+settings = Menu(menu_bar, tearoff=0)
+settings.add_command(label='Settings', command=open_settings)
+menu_bar.add_cascade(label='Settings', menu=settings)
+
 
 file_menu = Menu(menu_bar, tearoff=0)
 file_menu.add_command(label='Open', command=open_file)
@@ -334,19 +432,18 @@ root.config(menu=menu_bar)
 
 
 # Insert some Standard Text into the Edit Area
-editArea.insert('1.0', """print("Hello To The IDE-A")
+editArea.insert('1.0', """print("Welcome To The IDE-A")
 """)
 
 # Bind the KeyRelase to the Changes Function
-editArea.bind('<KeyRelease>', changes)
+#editArea.bind('<KeyRelease>', changes)
 editArea.bind('<Control-r>', run)
 editArea.bind('<Control-s>', save)
 editArea.bind('<Control-Alt-s>', save_as)
 
 
 
-
-changes()
+#hanges()
 root.mainloop()
 
 
--- main_windows.py
+++ main_windows.py
@@ -7,26 +7,29 @@ from tkinter.messagebox import showinfo
 import ctypes
 import re
 import os
+import idlelib.colorizer as ic
+import idlelib.percolator as ip
 from tkinter.filedialog import asksaveasfilename, askopenfilename, askdirectory
 import threading
-import customtkinter
+import customtkinter as ctk
 
 
 
 # Increas Dots Per inch so it looks sharper
 ctypes.windll.shcore.SetProcessDpiAwareness(True)
-customtkinter.set_appearance_mode("dark")  
-customtkinter.set_default_color_theme("blue")
+ctk.set_appearance_mode("dark")  
+ctk.set_default_color_theme("blue")
 
 
 
 # Setup Tkinter
-root = customtkinter.CTk()
+root = ctk.CTk()
 root.geometry('800x600')
 root.title("IDE-A (BETA 0.00001)")
 title = "IDE-A (Beta 0.00001)"
 root.state('normal')
-root.iconbitmap = "images\IDE_Logo.ico"
+img = PhotoImage(file='IDE.png')
+root.tk.call('wm', 'iconphoto', root._w, img)
 file_path = ''
 folder_path = ''
 
@@ -52,10 +55,10 @@ def save(s=0):
         f_path = asksaveasfilename(filetypes=[('Python Files', '*.py')])
     else:
         f_path = file_path
-    with open(path, 'w') as file:
+    with open(f_path, 'w') as file:
         code = editArea.get('1.0', END)
         file.write(code)
-        set_file_path(path)
+        set_file_path(f_path)
         root.title(f'IDE-A (Beta 0.00001) {file_path}')
 
 def save_as(s=0):
@@ -75,12 +78,11 @@ def run(s=0):
         save_prompt = Toplevel()
         text = Label(save_prompt, text='Please save your code')
         text.pack()
+        save()
         return
     else:
         #Windows one
         os.system(f'start cmd /K "python {file_path}"')
-        
-        #built in terminal (freezes)
         #terminal.entry.insert("end", f"python3 {file_path}")
 
         #for linux
@@ -91,10 +93,8 @@ def run(s=0):
 
 
 
-
-
 # Register Changes made to the Editor Content
-def changes(event=None):
+'''def changes(event=None):
     global previousText
 
     # If actually no changes have been made stop / return the function
@@ -115,8 +115,8 @@ def changes(event=None):
             i+=1
 
     previousText = editArea.get('1.0', END) 
-
-def search_re(pattern, text, groupid=0):
+'''
+'''def search_re(pattern, text, groupid=0):
     matches = []
 
     text = text.splitlines()
@@ -128,7 +128,7 @@ def search_re(pattern, text, groupid=0):
             )
 
     return matches
-
+'''
 
 def rgb(rgb):
     return "#%02x%02x%02x" % rgb
@@ -145,19 +145,20 @@ string = rgb((234, 162, 95))
 function = rgb((95, 211, 234))
 background = rgb((40, 41, 35))
 tree = rgb((42, 42, 48))
-
+variables = rgb((148, 215, 71))
 font = 'Consolas 15'
 
 
 # Define a list of Regex Pattern that should be colored in a certain way
-repl = [
+'''repl = [
     ['(^| )(False|None|True|and|as|assert|async|await|break|class|continue|def|del|elif|else|except|finally|for|from|global|if|import|in|is|lambda|nonlocal|not|or|pass|raise|return|try|while|with|yield)($| )', keywords],
     ['".*?"', string],
     ['\'.*?\'', string],
     ['#.*?$', comments],
-    ['print|input|', function]
+    ['print|input|', function],
+    #['str|int|byte|float', variables]
 ]
-
+'''
 
 
 ########################################
@@ -166,33 +167,42 @@ repl = [
 file_tree = Frame(root)
 file_tree.pack(side='left', fill=Y, expand=NO)
 def open_dir():
+    global abspath
     for i in tree.get_children():
         tree.delete(i)
     path = askdirectory()
     abspath = os.path.abspath(path)
     root_node = tree.insert('', 'end', text=abspath, open=True)
     process_directory(root_node,abspath)
-def process_directory( parent, path):
 
-        for p in os.listdir(path):
-            abspath = os.path.join(path, p)
-            isdir = os.path.isdir(abspath)
-            oid = tree.insert(parent, 'end', text=p, open=False)
-            if isdir:
-                process_directory(oid, abspath)
 
+filepaths = {}
+def process_directory(parent, path):
+    for p in os.listdir(path):
+        abspath = os.path.join(path, p)
+        isdir = os.path.isdir(abspath)
+        oid = tree.insert(parent, 'end', text=p, open=False)
+        filepaths[oid] = abspath  # save the full pathname
+        if isdir:
+            process_directory(oid, abspath)
 
 def Open_file_from_list_box(value):
-    global file
-    item_id = tree.selection()
-    file = tree.item(item_id, 'text') # get the filename from 'text' option
-    filepath = os.path.join(value,file)     
-    editArea.delete(1.0,END)
-    with open(filepath,"r") as f:
-        editArea.insert(1.0,f.read())
+    global file_path
+    file_path = ''
+    try:
+        item_id = tree.selection()[0]
+        file_path = filepaths[item_id] # get the full pathname
+        root.title(f"IDE-A (BETA 0.00001) {file_path}")
+        editArea.delete(1.0,END)
+        with open(file_path,"r") as f:
+            editArea.insert(1.0,f.read())
+    except Exception as ex:
+        print(ex.__class__.__name__, ex)
+
+
 
 tree = ttk.Treeview(file_tree)
-tree.pack(expand=YES,fill=BOTH)
+tree.pack(expand=YES, fill=BOTH)
 path = '.'
 tree.heading('#0', text=path, anchor='w')
 abspath = os.path.abspath(path)
@@ -209,16 +219,16 @@ terminal_frame.pack(side='bottom', fill='both')
 
 controll_panel = Frame(root, width=300, height=50, background='purple')
 controll_panel.pack(side='top', fill='x')
-Run_bttn = PhotoImage(file='images\play.png')
+Run_bttn = PhotoImage(file='play.png')
 img_label= Label(image=Run_bttn)
 
 #checks file_path continuosly
 def check_file_path():
     if file_path == '':
-        Run_bttn.config(file='images\disk.png')
+        Run_bttn.config(file='disk.png')
         control_bttn.config(command=save_as)
     else:
-        Run_bttn.config(file='images\play.png')
+        Run_bttn.config(file='play.png')
         control_bttn.config(command=run,)
     root.after(100, check_file_path)
 
@@ -226,8 +236,9 @@ def check_file_path():
 control_bttn= Button(controll_panel, image=Run_bttn, command=check_file_path,
 borderwidth=0)
 control_bttn.pack(side='right', pady=0.5)
-open_dir = Button(controll_panel,text="Open Directory",command=open_dir)
-open_dir.pack(side='left', fill='y')
+
+open_dir_bttn = Button(controll_panel,text="Open Directory",command=open_dir)
+open_dir_bttn.pack(side='left', fill='y')
 
 gapFrame = Frame(root, width=1)
 gapFrame.pack(side='left', fill='y')
@@ -249,6 +260,60 @@ editArea = Text(
 )
 
 editArea.pack(fill=BOTH, expand=1, side='right')
+
+cdg = ic.ColorDelegator()
+cdg.prog = re.compile(r'\b(?P<MYGROUP>tkinter)\b|' + ic.make_pat().pattern, re.S)
+cdg.idprog = r"(?<!class)\s+(\w+)"
+
+cdg.tagdefs['MYGROUP'] = {'foreground': '#7F7F7F', 'background': background}
+
+# These five lines are optional. If omitted, default colours are used.
+cdg.tagdefs['COMMENT'] = {'foreground': '#007F00 ', 'background': background}
+cdg.tagdefs['KEYWORD'] = {'foreground': '#27b1dd', 'background': background}
+cdg.tagdefs['BUILTIN'] = {'foreground': '#dddd22', 'background': background}
+cdg.tagdefs['STRING'] = {'foreground': '#8b9b40', 'background': background}
+cdg.tagdefs['DEFINITION'] = {'foreground': '#27b9b9', 'background': background}
+
+
+ip.Percolator(editArea).insertfilter(cdg)
+
+
+'''
+CHARBLUE = '#007F00 '
+CLOUD = '#4aded9 '
+CLOUD2 = '#47c2be '
+LEMON = '#e1ec34 '
+OVERCAST = '#dddd22 '
+PUMPKIN = '#ea9f13'
+STORMY = '#307ab4 '
+FIRE = '#ed7110 '
+DK_SEAFOAM = '#27b1dd '
+PURPLE = '#8623c2 '
+#test
+IDPROG = r"(?<!class)\s+(\w+)"
+
+TAGDEFS   = {   'COMMENT'    : {'foreground': CHARBLUE  , 'background': background},
+                'TYPES'      : {'foreground': CLOUD2    , 'background': background},
+                'NUMBER'     : {'foreground': LEMON     , 'background': background},
+                'BUILTIN'    : {'foreground': OVERCAST  , 'background': background},
+                'STRING'     : {'foreground': PUMPKIN   , 'background': background},
+                'DOCSTRING'  : {'foreground': STORMY    , 'background': background},
+                'EXCEPTION'  : {'foreground': CLOUD2    , 'background': background, 'font':'bold'},
+                'DEFINITION' : {'foreground': FIRE    , 'background': background, 'font':'bold'},
+                'DECORATOR'  : {'foreground': CLOUD2    , 'background': background, 'font':'italic'},
+                'INSTANCE'   : {'foreground': CLOUD     , 'background': background, 'font':'italic'},
+                'KEYWORD'    : {'foreground': DK_SEAFOAM, 'background': background, 'font':'bold'},
+                'CLASSDEF'   : {'foreground': PURPLE    , 'background': background, 'font':'bold'},
+            }
+
+cd         = ic.ColorDelegator()
+cd.idprog  = re.compile(IDPROG, re.S)
+cd.tagdefs = {**cd.tagdefs, **TAGDEFS}
+ip.Percolator(editArea).insertfilter(cd)'''
+
+
+
+
 
 class Terminal:
     def __init__(self, master):
@@ -292,7 +357,23 @@ class Terminal:
 terminal = Terminal(main_frame)
 
 
+def open_settings():
+    settings_window = ctk.CTk()
+    settings_window.title("IDE-A (Options)")
+    settings_window.geometry('600x400')
+    settings_window.state('normal')
 
+    string_label = ctk.CTkLabel(settings_window, text='Nothing is here dude :) \nYet...', pady=100, font=('Consolas', 15))
+    string_label.pack()
+    #string_label = ctk.CTkLabel(settings_window, text='Choose String color: ')
+    #string_label.pack()
+
+    #option_box = ctk.CTkOptionMenu(settings_window, values=["yellow", 'green', 'blue', 'red'])
+    #option_box.pack()
+    #option_box.set('yellow')
+
+
+    
 
 ########################################
 #GAP
@@ -302,14 +383,17 @@ gap.pack(side='left', fill='y', expand=NO)
 
 
 
-
-
 menu_bar = Menu(root)
 
 
 notes_bar = Menu(menu_bar, tearoff=0)
 notes_bar.add_command(label='PLZ NOTE THAT IF U CLICK "SAVE AS" THEN WRITE -|.py|- EXTENSION URSELF AT THE END')
 menu_bar.add_cascade(label='!NOTE!', menu=notes_bar)
+
+settings = Menu(menu_bar, tearoff=0)
+settings.add_command(label='Settings', command=open_settings)
+menu_bar.add_cascade(label='Settings', menu=settings)
+
 
 file_menu = Menu(menu_bar, tearoff=0)
 file_menu.add_command(label='Open', command=open_file)
@@ -335,18 +419,18 @@ root.config(menu=menu_bar)
 
 
 # Insert some Standard Text into the Edit Area
-editArea.insert('1.0', """print("Hello To The IDE-A")
+editArea.insert('1.0', """print("Welcome To The IDE-A")
 """)
 
 # Bind the KeyRelase to the Changes Function
-editArea.bind('<KeyRelease>', changes)
+#editArea.bind('<KeyRelease>', changes)
 editArea.bind('<Control-r>', run)
 editArea.bind('<Control-s>', save)
 editArea.bind('<Control-Alt-s>', save_as)
 
 
 
-
-changes()
+#hanges()
 root.mainloop()
+
 
